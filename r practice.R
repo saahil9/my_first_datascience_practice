1f5931060c96b49181c8c280354cfed7ab7ccea3
Saahil Borkar <83111937+saahil9@users.noreply.github.com>
2021-05-07 20:23:55 +0530

    Add files via upload

---
+++ r practice.R
@@ -0,0 +1,63 @@
+24*7
+2+3*4
+exp(3)
+sqrt(1909.5)
+pi
+pi*2
+14%%6
+xyz <- 45
+xyz
+abc <- FALSE
+abc
+ABC <- "saahil"
+ABC
+abc*2
+32/2
+3^2
+3^3
+exp (1)
+3^2
+2*pi*6378
+m <- 50
+x <- c(50,90,80,70)
+dbl_var <- c(20,30,"saahil")
+
+
+rep(3,8)
+seq (2,6)
+seq (2,10,by=2)
+
+x <- c(1,5,6,4)
+y <- c(1,2,2,4)
+x+y
+sqrt(x)
+y[5]
+
+
+x%%y
+y[4]
+y<4
+
+
+y[y<4]=1
+
+
+
+abc <- data.frame(x = 1:3 , y = c("a","b","c"))
+
+
+
+y
+
+
+
+
+abc[3,2] 
+
+DF <-data.frame(x=1:3 , y = c("a","b","c"))
+
+DF[1]
+DF[1,]
+DF[c(1,3),'y']
+DF[c(1,3),'x']
+DF[c(1,3),]
